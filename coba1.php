436ead5716e527f34c21f147693fcf9ad39a0a4f
azissiagiann <azisfahrisiagian@gmail.com>
2023-02-21 15:02:32 +0700

    ini tugas

---
+++ coba1.php
@@ -0,0 +1,9 @@
+<?php
+    define("PHI",3.14);
+    define("DBNAME", "inventori");
+    define("DBSERVER", "localhost");
+
+    echo "PHI nya : ".PHI;
+    echo "nama database nya : ".DBNAME;
+    echo "<br>Lokasi database nya : ".DBSERVER
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = "Azis Fahri Siagian" ;
+    $umur = "18" ;
+    $berat = "55" ;
+
+    echo "Nama saya : " .$nama;
+    echo "<br>Umur saya : " .$umur;
+    echo "<br>Berat saya : " .$berat;
+?>
